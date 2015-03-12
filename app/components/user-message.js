078e312d0bebe44b5d122aa6abe0e5694577431e
mramasubramanian <mramasubramanian@ingenio.com>
2015-03-11 17:12:11 -0700

    Moved the advisor replies HTML content to a user-message component and made the page to display some mock data

---
+++ app/components/user-message.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from '../templates/components/user-message';
+
+export default Ember.Component.extend({
+  layout: layout
+  //classNames: []
+});
--- app/routes/ask-keen.js
+++ app/routes/ask-keen.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(){
+    return {
+      messageFromAdvisors : [
+        {
+          id : 1,
+          profileImageUrl: "http://i.keen.com/ad-products.cdn.member75x75/22472422-1240184748.jpg",
+          advisorName: "Love Expert Sara",
+          postedDate: "Posted on March 10, 2015",
+          messageText: "Excellent feature! I love it. We are defintiely going to rock it on this friday!. Excellent feature! I love it. We are defintiely going to rock it on this friday!Excellent feature! I love it. We are defintiely going to rock it on this friday!"
+        },
+        {
+          id : 1,
+          profileImageUrl: "http://i.keen.com/ad-products.cdn.memberphotos/14123273-2128725806.jpg",
+          advisorName: "Psychic Answers By Candy",
+          postedDate: "Posted on March 10, 2015",
+          messageText: "Excellent feature! I love it. We are defintiely going to rock it on this friday!. Excellent feature! I love it. We are defintiely going to rock it on this friday!Excellent feature! I love it. We are defintiely going to rock it on this friday!"
+        }
+      ]
+    }
+  }
 });
