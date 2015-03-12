ea84083bdd2724026174c20ad61c7a937d89796f
mramasubramanian <mramasubramanian@ingenio.com>
2015-03-11 17:31:44 -0700

    Fixed the mock data along with the star rating.

--- app/components/user-message.js
+++ app/components/user-message.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 import layout from '../templates/components/user-message';
 
 export default Ember.Component.extend({
-  layout: layout
+  layout: layout,
+  starRatingImageUrl: function(){
+    return ('http://i.keen.com/D1_k3ratingstars-' + this.get("starRating") + '_V1.png');
+  }.property()
   //classNames: []
 });
--- app/routes/ask-keen.js
+++ app/routes/ask-keen.js
@@ -8,15 +8,17 @@ export default Ember.Route.extend({
           id : 1,
           profileImageUrl: "http://i.keen.com/ad-products.cdn.member75x75/22472422-1240184748.jpg",
           advisorName: "Love Expert Sara",
-          postedDate: "Posted on March 10, 2015",
-          messageText: "Excellent feature! I love it. We are defintiely going to rock it on this friday!. Excellent feature! I love it. We are defintiely going to rock it on this friday!Excellent feature! I love it. We are defintiely going to rock it on this friday!"
+          postedDate: "March 10, 2015",
+          messageText: "Excellent feature! I love it. We are defintiely going to rock it on this friday!. Excellent feature! I love it. We are defintiely going to rock it on this friday!Excellent feature! I love it. We are defintiely going to rock it on this friday!",
+          starRating: "5"
         },
         {
           id : 1,
           profileImageUrl: "http://i.keen.com/ad-products.cdn.memberphotos/14123273-2128725806.jpg",
           advisorName: "Psychic Answers By Candy",
-          postedDate: "Posted on March 10, 2015",
-          messageText: "Excellent feature! I love it. We are defintiely going to rock it on this friday!. Excellent feature! I love it. We are defintiely going to rock it on this friday!Excellent feature! I love it. We are defintiely going to rock it on this friday!"
+          postedDate: "March 10, 2015",
+          messageText: "Excellent feature! I love it. We are defintiely going to rock it on this friday!. Excellent feature! I love it. We are defintiely going to rock it on this friday!Excellent feature! I love it. We are defintiely going to rock it on this friday!",
+          starRating: "3"
         }
       ]
     }
