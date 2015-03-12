aa52e483ab234a3e58e0880cb1c2c6680ab7fb74
mramasubramanian <mramasubramanian@ingenio.com>
2015-03-12 14:52:05 -0700

    Cleaned up the notify methods used for test and have come up with event handlers which should be hooked later

--- client/app/controllers/dashboard.js
+++ client/app/controllers/dashboard.js
@@ -7,18 +7,31 @@ export default Ember.Controller.extend({
   init: function () {
     //Initialize push service object here?
   },
-  notificationTemplateFor:{
-    consumerTyping: "You will probably receive a live question related to topic 'LOVE'",
-    replyConsumerQuestion: "<h4>Question from our user:</h4>What does my future looks like. I've been going through a bad year that am totally lost. " +
-    "I need someone to help me here" +
-    "<br><div></div><textarea rows='4' cols='32'></textarea><button class='advisor-reply'>Reply</button></div>"
-  },
+
   actions: {
     notify: function(){
       var self = this;
+      //Test calls to be removed later since we have event handlers way down
       this.notify.success({raw: self.notificationTemplateFor.consumerTyping, closeAfter: null});
-      this.notify.success({raw: self.notificationTemplateFor.replyConsumerQuestion, closeAfter: null}, {closeAfter: null});
+      self.onQuestionArrives({question: 'What does my future looks like. I\'ve been going through a bad year that am totally lost. I need someone to help me here'});
     }
+  },
+
+  notificationTemplateFor:{
+    consumerTyping: "You will probably receive a live question related to topic 'LOVE'",
+    replyConsumerQuestion: "<h4>Question from our user:</h4>$question$<br><textarea rows='4' cols='32'></textarea><button class='advisor-reply'>Reply</button></div>"
+  },
+
+  //Event handlers
+  onQuestionArrives: function (data) {
+    var htmlContent = this.notificationTemplateFor.replyConsumerQuestion.replace('$question$',data.question);
+    this.notify.success({raw: htmlContent, closeAfter: null}, {closeAfter: null});
+  },
+
+  onConsumerTypesQuestion: function () {
+    var self = this;
+    this.notify.success(self.notificationTemplateFor.consumerTyping, {closeAfter: null});
   }
+
 
 });
