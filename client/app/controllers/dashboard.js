f210c52f278a8ccd30e6d3bcdaeeb120f8653763
Restuta <restuta8@gmail.com>
2015-03-12 23:27:35 -0700

    added "just-notify", hacky way to extend "ember-notify", but it works and code is cleaner, controller object is no longer available, use Notify

--- client/app/controllers/dashboard.js
+++ client/app/controllers/dashboard.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import Notify from '../utils/notify';
 
 export default Ember.ObjectController.extend({
   needs: ['application'],
@@ -14,6 +15,10 @@ export default Ember.ObjectController.extend({
       //Test calls to be removed later since we have event handlers way down
       this.notify.success({raw: self.notificationTemplateFor.consumerTyping, closeAfter: null});
       self.onQuestionArrives({question: 'What does my future looks like. I\'ve been going through a bad year that am totally lost. I need someone to help me here'});
+      Notify.success({closeAfter: null});
+      //Test calls to be removed later since we have event handlers way down
+      this.notify.success({raw: self.notificationTemplateFor.consumerTyping, closeAfter: null});
+      self.onQuestionArrives({question: 'What does my future looks like. I\'ve been going through a bad year that am totally lost. I need someone to help me here'});
       console.log(this.get('controllers.application.currentRouteName'));
       console.log(this.get('advisorId'));
     },
@@ -24,41 +29,23 @@ export default Ember.ObjectController.extend({
   },
 
   sockets: {
+    'consumer-started-typing': function() {
+      Notify.success({closeAfter: null});
+    },
+
     'new-question-posted': function(data) {
       console.log('new question posted');
-      var htmlContent = this.notificationTemplateFor.replyConsumerQuestion.replace('$question$', data.question);
-      this.notify.success({raw: htmlContent, closeAfter: null}, {closeAfter: null});
+      Notify.success({closeAfter: null});
     },
 
     'consumer-pressed-key': function(data) {
         this.set('questionText', data.value);
     }
   },
-
   //TODO: Muthu test answers to ask-keen page
   testAnswers: {
     answer1: "Excellent feature! I love it. We are defintiely going to rock it on this friday!. Excellent feature! I love it. We are defintiely going to rock it on this friday!Excellent feature! I love it. We are defintiely going to rock it on this friday!",
     answer2: "Can't say for sure, but I feel the presence of somebody important. It might be somebody from your past or nearest future. I need a little more info to tell for sure"
-  },
-
-  notificationTemplateFor: {
-    //Important!!: Ensure we dont modify the HTML hierarchy here. The hacky code on application.js has some dependency when dealing with finding and deleting elements
-    consumerTyping: "You will probably receive a live question related to topic 'LOVE'",
-    replyConsumerQuestion: "<h4>Question from our user:</h4>$question$<br><div><textarea rows='4' cols='32'></textarea><button class='advisor-reply'>Reply</button></div>"
-  },
-
-  //Event handlers
-  onQuestionArrives: function(data) {
-    //Show the form with reply only for dashboard page
-    if (this.get('controllers.application.currentRouteName') === 'dashboard') {
-    var htmlContent = this.notificationTemplateFor.replyConsumerQuestion.replace('$question$', data.question);
-    this.notify.success({raw: htmlContent, closeAfter: null}, {closeAfter: null});
-    }
-  },
-
-  onConsumerTypesQuestion: function() {
-    var self = this;
-    this.notify.success(self.notificationTemplateFor.consumerTyping, {closeAfter: null});
   }
 
 });
---
+++ client/app/utils/notify.js
@@ -0,0 +1,99 @@
+import Ember from 'ember';
+
+var MessagePromise = Ember.ObjectProxy.extend(Ember.PromiseProxyMixin);
+
+var Notify = Ember.Object.extend({
+
+  info: aliasToShow('info'),
+  success: aliasToShow('success'),
+  warning: aliasToShow('warning'),
+  alert: aliasToShow('alert'),
+  error: aliasToShow('error'),
+
+  init: function() {
+    console.log('notify.js init');
+    this.pending = [];
+  },
+
+  show: function(type, message, options) {
+    if (typeof message === 'object') {
+      options = message;
+      message = null;
+    }
+    message = Ember.merge({
+      message: message,
+      type: type
+    }, options);
+    var target = this.get('target');
+    var promise;
+    if (target) {
+      var view = target.show(message);
+      promise = Ember.RSVP.resolve(view);
+    }
+    else {
+      promise = new Ember.RSVP.Promise(function(resolve) {
+        this.pending.push({
+          message: message,
+          resolve: resolve
+        });
+      }.bind(this));
+    }
+    return MessagePromise.create({
+      message: message,
+      promise: promise
+    });
+  },
+
+  create: function(component) {
+    return Notify.create({
+      target: component
+    });
+  },
+
+  target: function(key, val) {
+    if (arguments.length === 2) {
+      this.showPending(val);
+    }
+    return val;
+  }.property(),
+
+  showPending: function(target) {
+    this.pending.map(function(pending) {
+      pending.resolve(target.show(pending.message));
+    });
+    this.pending = [];
+  }
+
+}).reopenClass({
+  // set to true to disable testing optimizations that are enabled when
+  // Ember.testing is true
+  testing: false
+});
+
+export default Notify.extend({
+  property: function() {
+    return Ember.computed(function() {
+      return Notify.create();
+    });
+  },
+  create: function() {
+    return Notify.create();
+  },
+  target: function(key, val) {
+    if (arguments.length === 2) {
+      Ember.assert("Only one {{ember-notify}} should be used without a source property. " +
+        "If you want more than one then use {{ember-notify source=someProperty}}",
+        !this._primary || this._primary.get('isDestroyed')
+      );
+      this.showPending(val);
+    }
+    return val;
+  }.property()
+
+}).create();
+
+function aliasToShow(type) {
+  return function(message, options) {
+    return this.show(type, message, options);
+  };
+}
