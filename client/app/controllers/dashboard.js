f503f0bc48d737e1d1d487aa8c1f15c707c3b4e5
mramasubramanian <mramasubramanian@ingenio.com>
2015-03-12 20:21:18 -0700

    Logic to show the notification popups only for the dashboard page

--- client/app/controllers/dashboard.js
+++ client/app/controllers/dashboard.js
@@ -1,46 +1,40 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  //newMessage: '',
-  //hasNewMessageArrived: false,
-
-  init: function() {
+  needs: ['application'],
+  init: function () {
     //Initialize push service object here?
   },
 
   actions: {
-    notify: function() {
+    notify: function(){
       var self = this;
       //Test calls to be removed later since we have event handlers way down
       this.notify.success({raw: self.notificationTemplateFor.consumerTyping, closeAfter: null});
       self.onQuestionArrives({question: 'What does my future looks like. I\'ve been going through a bad year that am totally lost. I need someone to help me here'});
+      console.log(this.get('controllers.application.currentRouteName'));
     }
   },
 
-  sockets: {
-    'new-question-posted': function(data) {
-      console.log('new question posted');
-      var htmlContent = this.notificationTemplateFor.replyConsumerQuestion.replace('$question$', data.question);
-      this.notify.success({raw: htmlContent, closeAfter: null}, {closeAfter: null});
-    }
-
-  },
-
-  notificationTemplateFor: {
+  notificationTemplateFor:{
     //Important!!: Ensure we dont modify the HTML hierarchy here. The hacky code on application.js has some dependency when dealing with finding and deleting elements
     consumerTyping: "You will probably receive a live question related to topic 'LOVE'",
     replyConsumerQuestion: "<h4>Question from our user:</h4>$question$<br><div><textarea rows='4' cols='32'></textarea><button class='advisor-reply'>Reply</button></div>"
   },
 
   //Event handlers
-  onQuestionArrives: function(data) {
-    var htmlContent = this.notificationTemplateFor.replyConsumerQuestion.replace('$question$', data.question);
-    this.notify.success({raw: htmlContent, closeAfter: null}, {closeAfter: null});
+  onQuestionArrives: function (data) {
+    //Show the form with reply only for dashboard page
+    if (this.get('controllers.application.currentRouteName') === 'dashboard') {
+      var htmlContent = this.notificationTemplateFor.replyConsumerQuestion.replace('$question$', data.question);
+      this.notify.success({raw: htmlContent, closeAfter: null}, {closeAfter: null});
+    }
   },
 
-  onConsumerTypesQuestion: function() {
+  onConsumerTypesQuestion: function () {
     var self = this;
     this.notify.success(self.notificationTemplateFor.consumerTyping, {closeAfter: null});
   }
+
 
 });
