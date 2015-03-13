e77d38681bcbeb380d1d3242aac4d4e0268e386a
muthur <muthunagaswamy@gmail.com>
2015-03-13 00:06:29 -0700

    Removign the mock data which we were shoing on the ask-keen page. From now on trigger answer from the dashboard only. For now use the "Send Answer1" 'Answer2" buttons on the dashboard which will emit events until Anton hooks the events on the notification popups.

--- client/app/controllers/ask-keen.js
+++ client/app/controllers/ask-keen.js
@@ -11,28 +11,31 @@ export default Ember.ObjectController.extend({
   respondingAdvisors: [],
   advisorsTyping: [],
   isQuestionSubmitted: false,
+  anyAnswerPresent: function(){
+    return (this.get('answersFromAdvisors').length > 0);
+  }.property('answersFromAdvisors.length'),
 
   init: function() {
-    //Muthu - just a temporay place to mock the incoming messages from advisors
-    this.onReceiveAnswer({
-      id: 1,
-      profileImageUrl: "http://i.keen.com/ad-products.cdn.member75x75/22472422-1240184748.jpg",
-      advisorId: 101,
-      advisorName: "Love Expert Sara",
-      postedDate: "March 10, 2015",
-      answer: "Excellent feature! I love it. We are defintiely going to rock it on this friday!. Excellent feature! I love it. We are defintiely going to rock it on this friday!Excellent feature! I love it. We are defintiely going to rock it on this friday!",
-      starRating: "5"
-    });
-
-    this.onReceiveAnswer({
-      id: 2,
-      profileImageUrl: "http://i.keen.com/ad-products.cdn.memberphotos/14123273-2128725806.jpg",
-      advisorId: 103,
-      advisorName: "Psychic Answers By Candy",
-      postedDate: "March 10, 2015",
-      answer: "Can't say for sure, but I feel the presence of somebody important. It might be somebody from your past or nearest future. I need a little more info to tell for sure.",
-      starRating: "4"
-    });
+    ////Muthu - just a temporay place to mock the incoming messages from advisors
+    //this.get('answersFromAdvisors').pushObject({
+    //  id: 1,
+    //  profileImageUrl: "http://i.keen.com/ad-products.cdn.member75x75/22472422-1240184748.jpg",
+    //  advisorId: 101,
+    //  advisorName: "Love Expert Sara",
+    //  postedDate: "March 10, 2015",
+    //  answer: "Excellent feature! I love it. We are defintiely going to rock it on this friday!. Excellent feature! I love it. We are defintiely going to rock it on this friday!Excellent feature! I love it. We are defintiely going to rock it on this friday!",
+    //  starRating: "5"
+    //});
+    //
+    //this.get('answersFromAdvisors').pushObject({
+    //  id: 2,
+    //  profileImageUrl: "http://i.keen.com/ad-products.cdn.memberphotos/14123273-2128725806.jpg",
+    //  advisorId: 103,
+    //  advisorName: "Psychic Answers By Candy",
+    //  postedDate: "March 10, 2015",
+    //  answer: "Can't say for sure, but I feel the presence of somebody important. It might be somebody from your past or nearest future. I need a little more info to tell for sure.",
+    //  starRating: "4"
+    //});
 
     this.get('advisorsTyping').push('muthudvisor');
     this.get('advisorsTyping').push('Anton advisor');
@@ -52,10 +55,16 @@ export default Ember.ObjectController.extend({
 
   sockets: {
     'new-advisor-answer': function(data) {
-      console.log('EVENT: new-advisor-answer');
+        console.log('EVENT: new-advisor-answer');
         console.log(data);
       //Add the incoming message data which inturn contain advisor info
         this.get('answersFromAdvisors').pushObject(data);
+    },
+    'advisor-started-typing': function(advisor){
+      console.log('EVENT: advisor-typing');
+      console.log(advisor);
+      //Add to a list of responding advisors
+      this.get('respondingAdvisors').pushObject(advisor.name);
     },
     connect: function() {
       console.log('Sockets connected...');
@@ -63,25 +72,11 @@ export default Ember.ObjectController.extend({
     disconnect: function() {
       console.log('Sockets disconnected...');
     }
-  },
-
-  /////////// Event handlers for push service events /////////////
-  /*This method will  be called when ever we receive an incoming message through push notifier*/
-  onReceiveAnswer: function(message) {
-    console.log(message);
-    this.get('answersFromAdvisors').pushObject(message);
-  },
-
-  whenAdvisorTypes: function(advisorName) {
-    console.log('Advisor ' + advisorName + ' typing');
-    //Add to a list of responding advisors
-    this.get('respondingAdvisors').pushObject(advisorName);
-
-  },
-
-  whenAdvisorStopsTyping: function(advisorName) {
-    console.log('Advisor ' + advisorName + 'stopped typing');
-    //Remove from the list respondingAdvisors
   }
+
+  //whenAdvisorStopsTyping: function(advisorName) {
+  //  console.log('Advisor ' + advisorName + 'stopped typing');
+  //  //Remove from the list respondingAdvisors
+  //}
 
 });
