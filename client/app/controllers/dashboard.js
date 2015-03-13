e322bbdc1527f6b9cbc3cef30dd359a8c8f6b656
Restuta <restuta8@gmail.com>
2015-03-12 22:19:58 -0700

    added questionText to dashboard.js

--- client/app/controllers/dashboard.js
+++ client/app/controllers/dashboard.js
@@ -4,12 +4,12 @@ export default Ember.ObjectController.extend({
   needs: ['application'],
   queryParams: ['advisorid'],
   advisorid: null,  //Need to have this property to match the case of querystring :(
-  init: function () {
+  init: function() {
     //Initialize push service object here?
   },
 
   actions: {
-    notify: function(){
+    notify: function() {
       var self = this;
       //Test calls to be removed later since we have event handlers way down
       this.notify.success({raw: self.notificationTemplateFor.consumerTyping, closeAfter: null});
@@ -23,28 +23,40 @@ export default Ember.ObjectController.extend({
     }
   },
 
+
+  sockets: {
+    'new-question-posted': function(data) {
+      console.log('new question posted');
+      var htmlContent = this.notificationTemplateFor.replyConsumerQuestion.replace('$question$', data.question);
+      this.notify.success({raw: htmlContent, closeAfter: null}, {closeAfter: null});
+    },
+
+    'consumer-pressed-key': function(data) {
+        this.set('questionText', data.value);
+    }
+
   //TODO: Muthu test answers to ask-keen page
   testAnswers: {
       answer1: "Excellent feature! I love it. We are defintiely going to rock it on this friday!. Excellent feature! I love it. We are defintiely going to rock it on this friday!Excellent feature! I love it. We are defintiely going to rock it on this friday!",
       answer2: "Can't say for sure, but I feel the presence of somebody important. It might be somebody from your past or nearest future. I need a little more info to tell for sure"
   },
 
-  notificationTemplateFor:{
+  notificationTemplateFor: {
     //Important!!: Ensure we dont modify the HTML hierarchy here. The hacky code on application.js has some dependency when dealing with finding and deleting elements
     consumerTyping: "You will probably receive a live question related to topic 'LOVE'",
     replyConsumerQuestion: "<h4>Question from our user:</h4>$question$<br><div><textarea rows='4' cols='32'></textarea><button class='advisor-reply'>Reply</button></div>"
   },
 
   //Event handlers
-  onQuestionArrives: function (data) {
+  onQuestionArrives: function(data) {
     //Show the form with reply only for dashboard page
     if (this.get('controllers.application.currentRouteName') === 'dashboard') {
-      var htmlContent = this.notificationTemplateFor.replyConsumerQuestion.replace('$question$', data.question);
-      this.notify.success({raw: htmlContent, closeAfter: null}, {closeAfter: null});
+    var htmlContent = this.notificationTemplateFor.replyConsumerQuestion.replace('$question$', data.question);
+    this.notify.success({raw: htmlContent, closeAfter: null}, {closeAfter: null});
     }
   },
 
-  onConsumerTypesQuestion: function () {
+  onConsumerTypesQuestion: function() {
     var self = this;
     this.notify.success(self.notificationTemplateFor.consumerTyping, {closeAfter: null});
   }
